23feff3d2505e4a3752c65f51c2b39c3f7bf215c
brcarey <brendan@weareadaptive.com>
2016-01-15 09:19:26 +0000

    forgot to send number of guests with the email

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import styles from './styles.css';
-import apostle from 'apostle.io';
-
-apostle.domainKey = "dd487e5521278b8fd8db25e5e69ec24643280c01";
-
-ReactDOM.render(<App />, document.getElementById('root'));
